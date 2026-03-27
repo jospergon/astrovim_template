53a88ed8ce1985c09a2e61ff241b00a71046662f
Jose <jose@mac.home>
2026-03-27 20:44:34 +0100

    Configure Ruby linting with rubocop and solargraph via none-ls and Mason

--- lua/polish.lua
+++ lua/polish.lua
@@ -3,3 +3,6 @@ if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+-- Foce rbenv to use 3.4.1
+vim.env.RBENV_VERSION = "3.4.1"
